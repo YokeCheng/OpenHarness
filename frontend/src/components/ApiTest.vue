e0b88286ae00c6ff4db38447554041b2c718b2f0
Yokecheng <18322535679@163.com>
2026-06-11 14:51:04 +0800

    feat: implement OpenHarness API client with SSE support

---
+++ frontend/src/components/ApiTest.vue
@@ -0,0 +1,29 @@
+// src/components/ApiTest.vue
+<template>
+  <div>
+    <button @click="testHealth">Test Health Check</button>
+    <p v-if="healthStatus">{{ healthStatus }}</p>
+  </div>
+</template>
+
+<script>
+import { healthCheck } from '@/api/openharness'
+
+export default {
+  data() {
+    return {
+      healthStatus: null
+    }
+  },
+  methods: {
+    async testHealth() {
+      try {
+        const result = await healthCheck()
+        this.healthStatus = JSON.stringify(result)
+      } catch (error) {
+        this.healthStatus = 'Error: ' + error.message
+      }
+    }
+  }
+}
+</script>
